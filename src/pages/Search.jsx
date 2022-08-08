8a1e77b4fab2815efdae03941237f79dd33f4ae3
Hian <hian.hsm@gmail.com>
2022-08-08 10:41:02 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      disabledButton: true,
+    };
+  }
+
+  handleInputLength = () => {
+    const minLength = 2;
+    const { input } = this.state;
+    const verifyLength = input.length < minLength;
+    this.setState({ disabledButton: verifyLength });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ input: target.value }, () => this.handleInputLength());
+  }
+
   render() {
+    const { disabledButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          placeholder="Nome do Artista"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ disabledButton }
+        >
+          Procurar
+
+        </button>
       </div>
     );
   }
